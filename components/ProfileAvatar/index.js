73adbb69c72b4d7de9652bee6ff7cac7cbaa3f68
wddipa <wdhanadipa@gmail.com>
2022-10-19 17:37:32 +0700

    added job card component'

--- components/ProfileAvatar/index.js
+++ components/ProfileAvatar/index.js
@@ -5,11 +5,11 @@ import { useState } from 'react';
 
 const ProfileAvatar = ({src, username}) => {
     return (
-        <div className='relative rounded-full w-9 p-[2px] h-9 ring flex justify-center items-center'>
+        <div className='relative rounded-full w-12 p-[2px] h-12 ring-2 ring-[#5C7CEA] flex justify-center items-center'>
             {src ? 
-            <Image src={src} alt={`${username} profile picture`} className="rounded-full" layout="fill"/> :
+            <Image src={src} alt={`${username} profile picture`} className="rounded-full" width={64} height={64} /> :
             username}
-            <div className='absolute -top-2 -right-2 bg-red-500 rounded-full w-4 h-4 text-xs text-center ring-[#5C7CEA] text-white'>1</div>
+            <div className='absolute -top-2 -right-2 bg-red-500 rounded-full w-5 h-5 text-xs text-center ring-[#5C7CEA] text-white flex justify-center items-center'>1</div>
         </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,39 @@
-import { ArrowLeft, CloseCircle, HambergerMenu } from "iconsax-react";
+import { Archive, ArrowLeft, Clock, CloseCircle, HambergerMenu, Location, Money, Spotify } from "iconsax-react";
 import Head from "next/head";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Avatar from "../components/Avatar";
+import CardJob from "../components/JobCard";
 import MobileHeader from "../components/MobileHeader";
 import ProfileAvatar from "../components/ProfileAvatar";
 
+const data =[
+  {
+    profile: '/img/company/spotify.png', 
+    positionLocation: 'Offsite, Silicon Valley',
+    position: 'Full Time', 
+    salaryRange: '3K-4K', 
+    description: 'We are looking for senior user interface designer with minimum 5 years experience in the field', 
+    title: 'Senior UI Designer', 
+    ownernName: 'Spotify AB', 
+    ownerLocation: 'Silicon Valley, california', 
+    heroImg: null, 
+    heroVideo: '/videos/interview.mp4',
+  }
+]
+
 export default function Home() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(false);
+  }, [])
 
   return (
     <>
     <MobileHeader profileImage={'/img/image.jfif'} username='HM'/>
     <div className='px-4'>
       <h6 className="font-bold text-2xl">Latest job</h6>
+      {data.map((item, key) => <CardJob key={key} {...item} loading={loading}/>)}
     </div>
     </>
   )
